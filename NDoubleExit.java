b8744610084def42ed591e36e9be32b818a22880
BeSingularTeam <besingulardemo@gmail.com>
2024-12-01 22:51:20 +0530

    commit

---
+++ NDoubleExit.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
