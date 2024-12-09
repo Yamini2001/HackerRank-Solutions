228286fc5cdf56b938fed98a2275d61dac903d5c
BeSingularTeam <besingulardemo@gmail.com>
2024-12-09 22:31:52 +0530

    commit

---
+++ Power.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double ans = 1.0;
+        long m = n;
+        if(m<0) m=-1*m;
+        while(m>0){
+            if(m%2==1){
+                ans = ans*x;
+                m = m-1;
+            }
+            else{
+                x = x*x;
+                m = m/2;
+            }
+        }
+        if(n<0) ans = (double)(1.0)/(double)(ans);
+        return ans;
+    }
+}
