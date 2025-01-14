ab7618bb07c1f704a1e6d5b2747b46c148676260
BeSingularTeam <besingulardemo@gmail.com>
2025-01-14 22:06:00 +0530

    update

---
+++ LengthOfLastWord.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        int index= s.length()-1;
+        while(index>=0 && s.charAt(index) == ' '){
+            index--;
+        }
+        while(index>=0 && s.charAt(index) != ' '){
+            length++;
+            index--;
+        }
+        return length;
+    }
+}
