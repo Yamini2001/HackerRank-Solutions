034468693e5ff3407b557f5cab9be68843b5a66c
BeSingularTeam <besingulardemo@gmail.com>
2025-01-25 22:16:46 +0530

    commit

---
+++ SortColors.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    int temp = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = temp;
+                }
+            }
+        }
+    }
+}
