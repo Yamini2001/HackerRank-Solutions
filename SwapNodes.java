2e30e79c88dbe3df813db5b421cce6ff4377e40a
BeSingularTeam <besingulardemo@gmail.com>
2025-03-08 22:50:25 +0530

    update

---
+++ SwapNodes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+         ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy, cur = head;
+
+        while (cur != null && cur.next != null) {
+            ListNode npn = cur.next.next;
+            ListNode second = cur.next;
+
+            second.next = cur;
+            cur.next = npn;
+            prev.next = second;
+
+            prev = cur;
+            cur = npn;
+        }
+
+        return dummy.next;  
+    }
+}
