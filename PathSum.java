dbd8d732f42af854b8ced044e7727e3f0c09bf9c
BeSingularTeam <besingulardemo@gmail.com>
2025-03-12 22:28:35 +0530

    update

---
+++ PathSum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+        
+        boolean leftSum = hasPathSum(root.left, targetSum - root.val);
+        boolean rightSum = hasPathSum(root.right, targetSum - root.val);
+        
+        return leftSum || rightSum;
+    }
+}
